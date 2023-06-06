5607bcfe37b7a8dbb7e242bb707fe34ba4eec9fa
stephen <stephen.stocking@gmail.com>
2023-06-06 09:27:10 -0400

    add loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import { Container, Grid, Text } from "@nextui-org/react";
+import { Container, Grid, Loading, Row, Text } from "@nextui-org/react";
 import axios from "axios";
 import React, { useCallback, useEffect, useState } from "react";
 import { ApiRequests } from "./types";
 
 function App() {
   const [data, setData] = useState<ApiRequests[]>([]);
+  const [fetching, setFetching] = useState(false);
 
   const getRequests = useCallback(async () => {
     try {
+      setFetching(true);
       const resp = await axios.get<ApiRequests[]>("https://pink-tense-oyster.cyclic.app/requests");
       const sortedData = resp.data.sort((a, b) => b.dateTime - a.dateTime);
       setData(sortedData);
     } catch (error) {
       console.log("OOPS", error);
+    } finally {
+      setFetching(false);
     }
   }, []);
 
@@ -21,8 +25,8 @@ function App() {
   }, [getRequests]);
 
   return (
-    <Container fluid className="h-screen bg-slate-500 m-0 " display="flex" direction="column" justify="center" alignItems="center">
-      <Text size="x-large" color="white" weight="bold">Availability at Stony Indian Creek campsite on 8/11 and 8/12</Text>
+    <Container fluid className="h-screen bg-slate-500 m-0 min-w-full" display="flex" direction="column" alignItems="center">
+      <Text size="x-large" color="white" weight="bold" className="mt-20">Availability at Stony Indian Creek campsite on 8/11 and 8/12</Text>
       <div className="max-w-[600px] min-w-[400px] mt-20">
         <Grid.Container>
           <Grid xs={6}>
@@ -41,7 +45,7 @@ function App() {
             </Text>
           </Grid>
         </Grid.Container>
-        <div className="max-h-[400px] overflow-y-auto bg-slate-50">
+        {!fetching ? <div className="max-h-[400px] overflow-y-auto bg-slate-50">
           <Grid.Container gap={2}>
             {data.map((req) => (
               <React.Fragment key={req.dateTime}>
@@ -57,7 +61,7 @@ function App() {
               </React.Fragment>
             ))}
           </Grid.Container>
-        </div>
+        </div> : <Row justify="center" className="mt-20"><Loading color="white"/></Row>}
       </div>
     </Container>
   );
